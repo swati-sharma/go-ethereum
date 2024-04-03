46857f0107f4cfcaa00a2cab959dd7e6ff866644
colinlyguo <colinlyguo@scroll.io>
2024-04-03 14:39:38 +0800

    tweak empty block logic

--- params/version.go
+++ params/version.go
@@ -23,9 +23,9 @@ import (
 
 const (
 	VersionMajor = 5         // Major version component of the current release
-	VersionMinor = 1         // Minor version component of the current release
-	VersionPatch = 31        // Patch version component of the current release
-	VersionMeta  = "4844-v0" // Version metadata to append to the version string
+	VersionMinor = 2         // Minor version component of the current release
+	VersionPatch = 1         // Patch version component of the current release
+	VersionMeta  = "4844-v1" // Version metadata to append to the version string
 )
 
 // Version holds the textual version string.
