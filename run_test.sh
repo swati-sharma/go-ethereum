dbbc883afd72b56a7c71c569c41e66b8fca1dd72
colinlyguo <colinlyguo@scroll.io>
2024-05-10 02:00:04 +0800

    remove cp to /usr/local/lib in CI

--- run_test.sh
+++ run_test.sh
@@ -2,12 +2,11 @@
 
 # Download .so files
 wget https://github.com/scroll-tech/da-codec/releases/download/v0.0.0-rc0-ubuntu20.04/libzktrie.so
-sudo mv libzktrie.so /usr/local/lib
 wget https://github.com/scroll-tech/da-codec/releases/download/v0.0.0-rc0-ubuntu20.04/libscroll_zstd.so
-sudo mv libscroll_zstd.so /usr/local/lib
 
 # Set the environment variable
-export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/lib
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$(pwd)
+export CGO_LDFLAGS="-L$(pwd) -lscroll_zstd -lzktrie"
 
 # Download and install the project dependencies
 go run build/ci.go install
