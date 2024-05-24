033e5b6e7f67a08c7f15daa457aaeb88894ad23a
HAOYUatHZ <haoyu@protonmail.com>
2024-05-24 19:01:59 +0800

    Revert "cmd/devp2p, eth: drop eth/66 (#28239)"
    This reverts commit bc6d184872889224480cf9df58b0539b210ffa9e.

--- eth/handler_eth.go
+++ eth/handler_eth.go
@@ -67,7 +67,7 @@ func (h *ethHandler) Handle(peer *eth.Peer, packet eth.Packet) error {
 	case *eth.NewBlockPacket:
 		return h.handleBlockBroadcast(peer, packet.Block, packet.TD)
 
-	case *eth.NewPooledTransactionHashesPacket67:
+	case *eth.NewPooledTransactionHashesPacket66:
 		return h.txFetcher.Notify(peer.ID(), nil, nil, *packet)
 
 	case *eth.NewPooledTransactionHashesPacket68:
@@ -81,7 +81,7 @@ func (h *ethHandler) Handle(peer *eth.Peer, packet eth.Packet) error {
 		}
 		return h.txFetcher.Enqueue(peer.ID(), *packet, false)
 
-	case *eth.PooledTransactionsResponse:
+	case *eth.PooledTransactionsPacket:
 		return h.txFetcher.Enqueue(peer.ID(), *packet, true)
 
 	default:
